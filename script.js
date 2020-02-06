c38ff6c7fd85d493ef6a2e609504934e038e125d
James Dobbe <dobbe2@gmail.com>
2020-02-06 15:40:04 -0800

    updated cards, condensed/cleaned css file, fixed footer, added to about me page

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-    $('#local-tunes-port').on('click', function(){
-        $('#local-tunes-info').empty();
-        $("#local-tunes-info").append(`<p>Local Tunes is a music aggregation tool for each country's top tracks based off of user location, or user input.
-        <br>This was done as a group project with classmates. 
-        <br>With Local Tunes, you are able to get a stylized list of top songs from either your current location, or from foriegn counties.
-        <br>Frameworks and Libraries used are: <ul>Material.io (A CSS Framework)</ul><ul>Google Maps API</ul><ul>MusixMatch API</ul><ul>Last.FM API</ul>
-    </p>`);
+    $("#local-pic").click(function(){
+        $("#local-pic").attr('src', 'assets/images/localTunes.png');
     })
 
-})
+
+});
+//     $('#local-tunes-port').on('click', function(){
+//         $('#local-tunes-info').empty();
+//         $("#local-tunes-info").append(`<p>Local Tunes is a music aggregation tool for each country's top tracks based off of user location, or user input.
+//         <br>This was done as a group project with classmates. 
+//         <br>With Local Tunes, you are able to get a stylized list of top songs from either your current location, or from foriegn counties.
+//         <br>Frameworks and Libraries used are: <ul>Material.io (A CSS Framework)</ul><ul>Google Maps API</ul><ul>MusixMatch API</ul><ul>Last.FM API</ul>
+//     </p>`);
+//     })
+
+// })
 
 
